c295e8243a0c626e5c0cf9d84c1f1f6939c97893
Corri Atkinson <corri.atkinson@gmail.com>
2025-09-21 12:12:52 -0400

    Add forest.sh and mojave.sh installation scripts

---
+++ forest.sh
@@ -0,0 +1,4 @@
+sudo ./install.sh -t forest -p window -i right -b
+#sudo ./install.sh -t forest -p window -i right -b
+
+
---
+++ mojave.sh
@@ -0,0 +1,2 @@
+#sudo ./install.sh -t mojave -p blur -i right -c light -s 4k -b
+sudo ./install.sh -r -t mojave -p blur -i right -c light -s 4k -b
